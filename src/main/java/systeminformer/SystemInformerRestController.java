99ed49254804b6181ad94c3e2841d658dbbe41c8
jemag <alexandre.bd@tutanota.com>
2022-06-20 16:00:30 -0400

    try adding codeql vulnerability

--- src/main/java/systeminformer/SystemInformerRestController.java
+++ src/main/java/systeminformer/SystemInformerRestController.java
@@ -1,14 +1,22 @@
 package systeminformer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
 
 @RestController
 
 public class SystemInformerRestController {
     private final String DATE_FORMAT = "yyyy/MM/dd HH:mm";
+    @Autowired
+    private DataSource dataSource;
 
     @RequestMapping("/info")
     public InformationDto getInfo() throws Exception {
@@ -17,5 +25,17 @@ public class SystemInformerRestController {
         dto.setIpAddress(InetAddress.getLocalHost().getHostAddress());
         int intentionalPmdViolation = 9;
         return dto;
+    }
+
+    @RequestMapping("/sql")
+    public void getSql(@RequestParam(value = "text") String text) throws Exception {
+      String sql = "select "
+        + "customer_id,acc_number,branch_id,balance "
+        + "from Accounts where customer_id = '"
+        + text 
+        + "'";
+      Connection c = this.dataSource.getConnection();
+      ResultSet rs = c.createStatement().executeQuery(sql);
+    // ...
     }
 }
