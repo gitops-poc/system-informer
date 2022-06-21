d33ca6050470065a14c62184197553556632a75e
jemag <alexandre.bd@tutanota.com>
2022-06-21 17:41:12 -0400

    fix checkstyle errors

--- src/main/java/systeminformer/InformationDto.java
+++ src/main/java/systeminformer/InformationDto.java
@@ -1,22 +1,22 @@
 package systeminformer;
 
-public class InformationDto {
-    private String hostname;
-    private String ipAddress;
+public final class InformationDto {
+  private String hostname;
+  private String ipAddress;
 
-    public String getHostname() {
-        return hostname;
-    }
+  public String getHostname() {
+    return hostname;
+  }
 
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
+  public void setHostname(final String hostname) {
+    this.hostname = hostname;
+  }
 
-    public String getIpAddress() {
-        return ipAddress;
-    }
+  public String getIpAddress() {
+    return ipAddress;
+  }
 
-    public void setIpAddress(String ipAddress) {
-        this.ipAddress = ipAddress;
-    }
+  public void setIpAddress(final String ipAddress) {
+    this.ipAddress = ipAddress;
+  }
 }
